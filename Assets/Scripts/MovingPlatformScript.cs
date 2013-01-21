4c733d196664fb97ef2196a18571be3cfbade97f
Jean-Vincent Lamberti <jv.lamberti@live.fr>
2013-01-21 15:47:42 +0100

    Fix : Scene level_two

--- Assets/Scripts/MovingPlatformScript.cs
+++ Assets/Scripts/MovingPlatformScript.cs
@@ -1,54 +1,43 @@
 using UnityEngine;
 using System.Collections;
 
-public class MovingHPlatformScript : MonoBehaviour {
+public class MovingPlatformScript : MonoBehaviour {
 
     public float minIncrement;
     public float maxIncrement;
     public enum axisEnum { x, y, z };
     public axisEnum axis;
-    private float relPos;
-    public float decrement;
+    public float speed;
     private Vector3 initialPosition;
     private WorldControlerScript worldControler;
     private int currentWorldNumber;
     private Vector3 relPosition;
     private Vector3[] translations;
 
-
 	// Use this for initialization
 	void Start () {
         initialPosition = gameObject.transform.position;
         worldControler = GameObject.Find("GameWorld").GetComponent<WorldControlerScript>();
         currentWorldNumber = worldControler.getCurrentWorldNumber();
-        /*minIncrement = 10.0f;
-        maxIncrement = 10.0f;*/
-        //axis = 'x';       
-        //decrement = 0.2f;
         relPosition.Set(0, 0, 0);
         translations = new Vector3[3];
-        translations[0].Set(decrement, 0, 0);
-        translations[1].Set(0, decrement, 0);
-        translations[2].Set(0, 0, decrement);
+        translations[0].Set(speed, 0, 0);
+        translations[1].Set(0, speed, 0);
+        translations[2].Set(0, 0, speed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         currentWorldNumber = worldControler.getCurrentWorldNumber();
-        if (currentWorldNumber == 0)
-        {
-           
-                if (relPosition.x < minIncrement && relPosition.y < minIncrement && relPosition.z < minIncrement)
-                {
-                    gameObject.transform.Translate(translations[(int)axis]);
-                }
-
-            relPosition = gameObject.transform.position + initialPosition;
+    
+        if (currentWorldNumber == 0) {
+            if (relPosition[(int)axis] < minIncrement) {
+                gameObject.transform.Translate(translations[(int)axis]);
+                relPosition = gameObject.transform.position + initialPosition;
+            }
         }
-        else
-        {
-            if (relPosition.x > maxIncrement * -1 && relPosition.y > maxIncrement * -1 && relPosition.z > maxIncrement * -1)
-            {
+        else {
+            if (relPosition[(int)axis] > maxIncrement * -1) {
                 gameObject.transform.Translate(-1*translations[(int)axis]);
                 relPosition = gameObject.transform.position + initialPosition;
             }
